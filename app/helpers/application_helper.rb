179432cf1f82edcb621eb1f3244b466e3b6ea0c8
Michael Slone <m.slone@gmail.com>
2013-06-18 08:14:01 -0400

    override helper to fix bad layout when some fields are undefined

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_field_value value=nil
+    value = [value] unless value.is_a? Array
+    value = value.collect { |x| x.respond_to?(:force_encoding) ? x.force_encoding("UTF-8") : x}
+    return value.collect { |x|
+      if x.to_s.strip == ''
+        '&nbsp;'
+      else
+        html_escape x
+      end
+    }.join(field_value_separator).html_safe
+  end
 end
