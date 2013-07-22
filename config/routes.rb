f7302fccfb08bd85407c199a42d991f08c2a8ab1
Michael Slone <m.slone@gmail.com>
2013-07-22 10:06:19 -0400

    revert some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Presence::Application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_for :users
   resources :user, :controller => "user"
   resources :packages do
     resources :assets do
